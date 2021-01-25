b2c202212f235b744d479fdc91f7755acf0fae4e
Freewind <nowindlee@gmail.com>
2021-01-25 17:35:34 +0800

    typescript-react-forward-ref-demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-TypeScript React Hello World Demo
+TypeScript React Forward Ref Demo
 =================================
+
+使用React.forwardRef可以把一个组件内部引用暴露给外部，在某些情况下有用。
+
+如果不使用`React.forwardRef`，也可以自己定义一个`ref` property做同样的事情，不过需要自己指定类型等，相对麻烦一些
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,9 +1,21 @@
-import React, { FC } from 'react';
+import React, {FC, useRef, useState} from 'react';
+import {MyComponent} from './MyComponent';
 
 type Props = {};
 
 export const Hello: FC<Props> = ({}) => {
+  const ref = useRef<HTMLDivElement | null>(null)
+
+  const [html, setHtml] = useState<string>()
+
+  function getHtml() {
+    setHtml(ref.current?.outerHTML ?? '')
+  }
+
   return <div>
-    <h1>Hello React</h1>
+    <MyComponent ref={ref} defaultValue={'react'}/>
+    <button onClick={() => getHtml()}>Get Html</button>
+    <hr/>
+    {html}
   </div>;
 }
